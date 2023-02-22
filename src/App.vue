7deee794aa415bae99e39153e559db183f031e36
kim myeong jong「キム・ミョンジョン」 <55441440+tolerancable-code@users.noreply.github.com>
2023-02-23 08:32:53 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-main>
+      <NavBar />
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/public/NavBar.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    NavBar,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
